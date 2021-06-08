932f6a22457378f83e9ec7aa7b418c34b89df55b
Alexander <satan1c@ukr.net>
2021-06-08 16:13:37 +0300

    1.0.0

---
+++ DSharpPlus/HoloCS.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using DSharpPlus;
+using HoloCS.DSharpPlus.Types;
+using Newtonsoft.Json;
+
+namespace HoloCS.DSharpPlus
+{
+    public class HoloClient
+    {
+        internal static readonly TimeSpan RateLimit = new TimeSpan(0, 0, 0, 5);
+        internal static DateTime LastRequest;
+
+        internal static readonly HttpClient HttpClient = new HttpClient
+        {
+            BaseAddress = new Uri("http://discord-holo-api.ml/api/")
+        };
+
+        public HoloClient(DiscordClient client)
+        {
+            Client = client;
+
+            SFW = new Sfw();
+            HttpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+        
+        public Sfw SFW { get; set; }
+
+        internal string Token { get; set; }
+        internal static DiscordClient Client { get; set; }
+
+        internal static async Task<T> GetRequest<T>(string path)
+        {
+            await RateLimiter();
+
+            var response = await HttpClient.GetAsync(path);
+            response.EnsureSuccessStatusCode();
+
+            LastRequest = DateTime.UtcNow;
+
+            return JsonConvert.DeserializeObject<T>(await response.Content.ReadAsStringAsync());
+        }
+
+        internal static async Task<T> GetRequest<T>(string path, HttpClient client, TimeSpan timeout = new TimeSpan())
+        {
+            await RateLimiter(timeout != new TimeSpan() ? timeout : new TimeSpan(0, 0, 0, 0, 50));
+
+            var response = await client.GetAsync(path);
+            response.EnsureSuccessStatusCode();
+
+            LastRequest = DateTime.UtcNow;
+
+            return JsonConvert.DeserializeObject<T>(await response.Content.ReadAsStringAsync());
+        }
+
+        internal static async Task RateLimiter()
+        {
+            while (true)
+            {
+                if (LastRequest == new DateTime() || DateTime.UtcNow - LastRequest > RateLimit) return;
+                await Task.Delay((int) MathF.Round((float) ((DateTime.UtcNow - LastRequest) - RateLimit).TotalMilliseconds, 0));
+            }
+        }
+
+        internal static async Task RateLimiter(TimeSpan timeout)
+        {
+            while (true)
+            {
+                if (LastRequest == new DateTime() || DateTime.UtcNow - LastRequest > timeout) return;
+                await Task.Delay((int) MathF.Round((float) ((DateTime.UtcNow - LastRequest) - timeout).TotalMilliseconds, 0));
+            }
+        }
+    }
+}
---
+++ DSharpPlus/Types/SFW.cs
@@ -0,0 +1,64 @@
+﻿using System.Threading.Tasks;
+
+namespace HoloCS.DSharpPlus.Types
+{
+    public class Sfw
+    {
+        internal Sfw()
+        {
+        }
+        
+        public async Task<HoloResponce> GetImage(string category)
+        {
+            return await HoloClient.GetRequest<HoloResponce>(category);
+        }
+        
+        public  async Task<HoloResponce> GetImage(Categories category)
+        {
+            return await HoloClient.GetRequest<HoloResponce>(category.ToString());
+        }
+    }
+
+    public class HoloResponce
+    {
+        /// <summary>
+        ///     An error message, != null only when error
+        /// </summary>
+        public string message { get; set; }
+        
+        /// <summary>
+        ///     An image url, != null if 
+        /// </summary>
+        public string url { get; set; }
+    }
+
+    public enum Categories
+    {
+        art,
+        ask,
+        bite,
+        cry,
+        cuddle,
+        dance,
+        ego,
+        glare,
+        highfive,
+        hug,
+        kiss,
+        lick,
+        nom,
+        pat,
+        poke,
+        pressf,
+        punch,
+        sex,
+        shoot,
+        slap,
+        slappope,
+        smug,
+        suicide,
+        tickle,
+        wasted,
+        wink
+    }
+}
